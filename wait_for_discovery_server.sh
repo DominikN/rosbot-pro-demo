1febd28d40b36ceb9ed1e1aeed0718e8598e71df
dominikn <dominik.nwk@gmail.com>
2022-03-07 17:31:17 +0100

    debugging dds routerg

---
+++ wait_for_discovery_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Waiting for \"rosbot\" host to be available in /etc/hosts"
+
+while [[ $(grep 'rosbotr' /etc/hosts | wc -l) -eq 0 ]]; do 
+    sleep 1
+done
+
+sleep 2
+
+echo "\"rosbot\" present in /etc/hosts:"
+
+# print the IPv6 address of the Discovery Server
+grep 'rosbot' /etc/hosts
+
+echo "Ready to launch ROS 2 nodes"
